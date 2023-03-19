b1bead2d4b37edc8b788bc92c63dee407fc924bf
kmccabe1 <kmccabe1@email.unc.edu>
2023-03-19 10:32:02 -0400

    Updated make_dataset structure
    Removed usage of os and incorporated python-click for CLI

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -1,48 +1,52 @@
-import os,sys,shutil
+import click
+import shutil
+from pathlib import Path
 from PIL import Image
-import numpy as np
 
-DATA_TYPE = ['png','PNG','jpg','JPG']
-def create_dataset():
-    USAGE = "USAGE: python path/to/make_dataset.py path/to/raw/images dataset_name path/to/CIHP_PGN"
-    if len(sys.argv)!=4 or '--help' in sys.argv or '-h' in sys.argv:
-        print(USAGE)
-        os._exit(0)
-    print("Generating Dataset .....")
-    # path to raw images
-    path_img = sys.argv[1]
-    # name for created dataset
-    name_dataset = sys.argv[2]
-    # place dataset in CIHP_PGN
-    path_dataset = os.path.join(sys.argv[3], 'datasets', name_dataset)
-    if os.path.isdir(path_dataset):
-        # Delete directory in CIHP_PGN 
-        shutil.rmtree(path_dataset)
-        print('Dataset Already Exists')
-    os.makedirs(path_dataset)
-    path_edge = os.path.join(path_dataset, 'edges')
-    path_images = os.path.join(path_dataset, 'images')
-    path_list = os.path.join(path_dataset, 'list')
+DATA_TYPE = [".png", ".jpg"]
+
+
+@click.command()
+@click.argument("src")
+@click.argument("name")
+@click.argument("dst")
+@click.option("--delete", is_flag=True, default=False, help="Delete dst directory")
+def create_dataset(src, name, dst, delete):
+    """Creates a dataset from raw images for CIHP_PGN"""
+    click.echo("Generating dataset ...")
+    # Create output path for dataset
+    path_dataset = Path(dst, "datasets", name)
+    if Path(path_dataset).is_dir():
+        if delete:
+            # Delete dst directory
+            shutil.rmtree(path_dataset)
+        else:
+            click.echo("Dataset already exists")
+            return
+    path_edge = Path(path_dataset, "edges")
+    path_images = Path(path_dataset, "images")
+    path_list = Path(path_dataset, "list")
     for p in [path_edge, path_images, path_list]:
-        os.makedirs(p)
-    files = [i for i in os.listdir(path_img) if i.split('.')[-1] in DATA_TYPE]
+        Path.mkdir(p, parents=True)
+    files = [i for i in Path(src).iterdir() if i.suffix in DATA_TYPE]
+
     for f in files:
-        im = Image.open(os.path.join(path_img, f))
-        #im = im.resize((im.size[0]*720//im.size[1],720), Image.LANCZOS) # if you run out of GPU memory
-        im1 = Image.new('L', im.size)
+        im = Image.open(f)
+        # im = im.resize((im.size[0]*720//im.size[1],720), Image.LANCZOS) # if you run out of GPU memory
+        im1 = Image.new("L", im.size)
         im = im.rotate(90)
         im1 = im1.rotate(90)
-        im.save(os.path.join(path_images, f))
-        im1.save(os.path.join(path_edge, '.'.join(f.split('.')[:-1])+'.png'))
-    with open(os.path.join(path_list, 'val.txt'), 'w') as flist:
+        im.save(Path(path_images, f.name))
+        im1.save(Path(path_edge, f.stem + ".png"))
+    with open(Path(path_list, "val.txt"), "w") as flist:
         for f in files:
-            flist.write('/images/%s /edges/%s\n'%(f,'.'.join(f.split('.')[:-1])+'.png'))
-    with open(os.path.join(path_list, 'val_id.txt'), 'w') as flist:
+            flist.write("/images/%s /edges/%s\n" % (f.name, f.name))
+    with open(Path(path_list, "val_id.txt"), "w") as flist:
         for f in files:
-            flist.write('%s\n'%'.'.join(f.split('.')[:-1]))
+            flist.write("%s\n" % (f.stem))
 
-    print('Dataset Completed!')
+    click.echo("Dataset Completed!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_dataset()
