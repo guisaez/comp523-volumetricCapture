815adcb187f5e965c86d898408fedba6f0595215
Guillermo Saez <guisaez@email.unc.edu>
2023-04-08 14:11:18 -0400

    Adding Project Service and NATS Streaming

---
+++ projects-service/src/events/listeners/__test__/file-updated-listener.test.ts
@@ -0,0 +1,73 @@
+import { FileUpdatedListener } from "../file-updated-listener";
+import { FileUpdatedEvent, FileTypes } from "@teamg2023/common";
+import { File } from "../../../models/file";
+import mongoose from "mongoose";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+
+    const listener = new FileUpdatedListener(natsWrapper.client);
+
+    const file = await File.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        mimetype: 'text/yml',
+        encoding: 'utf-8',
+        type: FileTypes.INTRINSIC,
+        userId: new mongoose.Types.ObjectId().toHexString(),
+        name: 'intri.yml'
+    })
+
+    await file.save();
+
+    const data: FileUpdatedEvent['data'] = {
+        id: file.id,
+        version: file.version + 1,
+        name: 'intri_updated.yml',
+        mimetype: file.mimetype,
+        type: file.type,
+        userId: file.userId,
+        encoding: file.encoding,
+    }
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    }
+
+    return { listener, data, file, msg };
+}
+
+it('finds, updates and saves a file', async () => {
+    const { listener, data, msg, file } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    const updatedTicket = await File.findById(file.id);
+
+    expect(updatedTicket).toBeDefined();
+    expect(updatedTicket!.version).toEqual(data.version);
+    expect(updatedTicket!.name).toEqual(data.name);
+});
+
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+})
+
+it('does not call ack if the event has a skipped version number', async () => {
+    const { listener, data, msg } = await setup();
+
+    data.version = 10;
+
+    try{
+        await listener.onMessage(data, msg);
+    }catch(err){
+
+    }
+    
+    expect(msg.ack).not.toHaveBeenCalled();
+});
---
+++ projects-service/src/models/project.ts
@@ -0,0 +1,92 @@
+import mongoose from 'mongoose';
+import { ProcessStatus } from '@teamg2023/common';
+import { FilesDoc} from './file';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+export { ProcessStatus };
+
+interface ProjectAttrs {
+    userId: string;
+    projectName: string;
+}
+
+interface ProjectDoc extends mongoose.Document {
+    userId: string;
+    projectName: string;
+    createdAt: Date;
+    lastModifiedAt: Date;
+    version: number;
+    processStatus: ProcessStatus;
+    zip_fileId: FilesDoc;
+    extrinsic_fileId: FilesDoc;
+    intrinsic_fileId: FilesDoc;
+    output_fileId: FilesDoc;
+}
+
+interface ProjectModel extends mongoose.Model<ProjectDoc> {
+    build(attrs: ProjectAttrs): ProjectDoc;
+    findByEvent(event: {projectId: string}): Promise<ProjectDoc | null>;
+}
+
+const projectSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    projectName: {
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: new Date()
+    },
+    lastModifiedAt: {
+        type: Date,
+        default: new Date()
+    },
+    processStatus: {
+        type: String,
+        enum: Object.values(ProcessStatus),
+        default: ProcessStatus.NotStarted
+    },
+    zip_fileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    },
+    extrinsic_fileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    },
+    intrinsic_fileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    },
+    output_fileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+        }
+    }
+})
+
+projectSchema.set('versionKey', 'version');
+projectSchema.plugin(updateIfCurrentPlugin);
+
+projectSchema.statics.build = (attrs: ProjectAttrs) => {
+    return new Project(attrs);
+}
+
+projectSchema.statics.findByEvent = (event: {projectId: string}) => {
+    return Project.findOne({
+        _id: event.projectId
+    });
+}
+const Project = mongoose.model<ProjectDoc, ProjectModel>('Project', projectSchema);
+
+export { Project }
