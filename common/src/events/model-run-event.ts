f20aaa34e5ad65b9e8822e51498948f67a97cee8
Guillermo Saez <guisaez@email.unc.edu>
2023-04-12 11:17:19 -0400

    Updates

--- common/src/events/model-run-event.ts
+++ common/src/events/model-run-event.ts
@@ -2,19 +2,13 @@ import { Subjects } from "./subjects";
 import { FileTypes } from "./types/file-types";
 
 export interface RunData {
-    files: [
-        {
-            fileId: string,
-            name: string,
-            type: FileTypes
-        }
-    ]
+        fileId: string;
+        name: string;
+        type: FileTypes;
 }
 
 export interface ModelRunEvent {
     subject: Subjects.ProcessStarted;
 
-    data: {
-        files: RunData
-    }
+    data: RunData[];
 }
