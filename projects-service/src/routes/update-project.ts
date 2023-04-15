47de872fa2a331e24cc2605a373f387a3b4e3270
Jianghao Wu <wujianghao0706@gmail.com>
2023-04-14 22:30:33 -0400

    v0.61
    format update

--- projects-service/src/routes/update-project.ts
+++ projects-service/src/routes/update-project.ts
@@ -7,34 +7,34 @@ import { body } from 'express-validator';
 const router = express.Router();
 
 router.patch('/api/projects/:id', requireAuth, [
-    body('projectName')
-        .isString()
-        .isLength({
-            min: 1
-        })
-        .not()
-        .isEmpty()
-        .withMessage('Project Name cannot be empty')
-], validateRequest, async (req: Request, res: Response ) => {
-    if(!mongoose.isObjectIdOrHexString(req.params.id)){
-        throw new BadRequestError('Invalid Project Id');
-    }
+  body('projectName')
+    .isString()
+    .isLength({
+      min: 1
+    })
+    .not()
+    .isEmpty()
+    .withMessage('Project Name cannot be empty')
+], validateRequest, async (req: Request, res: Response) => {
+  if (!mongoose.isObjectIdOrHexString(req.params.id)) {
+    throw new BadRequestError('Invalid Project Id');
+  }
 
-const { projectName } = req.body;
-const projectId = req.params.id;
+  const { projectName } = req.body;
+  const projectId = req.params.id;
 
-// Update the project's name
-const updatedProject = await Project.findByIdAndUpdate(
-  projectId,
-  { projectName },
-  { new: true } // Set { new: true } to return the updated project
-);
+  // Update the project's name
+  const updatedProject = await Project.findByIdAndUpdate(
+    projectId,
+    { projectName },
+    { new: true } // Set { new: true } to return the updated project
+  );
 
-if (!updatedProject) {
-  throw new BadRequestError('Project Not Found');
-}
+  if (!updatedProject) {
+    throw new BadRequestError('Project Not Found');
+  }
 
-res.status(200).send(updatedProject);
+  res.status(200).send(updatedProject);
 })
 
 export { router as projectUpdateRouter };
