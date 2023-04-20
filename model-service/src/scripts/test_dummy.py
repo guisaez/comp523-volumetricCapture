2632744d90b3795a4d69b1ccd08ee84ff0bb641d
Guillermo Saez <guisaez@email.unc.edu>
2023-04-20 17:24:34 -0400

    Model Service

---
+++ model-service/src/scripts/test_dummy.py
@@ -0,0 +1,13 @@
+import time
+
+
+def test_dummy():
+    for i in range(1):
+        print("Loop iteration", i)
+        time.sleep(40)
+    
+    return "e234322dee32243123"
+
+if __name__ == "__main__":
+    test_dummy()
+    
