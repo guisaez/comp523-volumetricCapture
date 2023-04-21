7937d2c3d3b0c097e20ff9e1ef07e866cc4fe18a
Guillermo Saez <guisaez@email.unc.edu>
2023-04-21 19:11:22 -0400

    Modifying scripts for automation

--- model-service/src/scripts/automation.py
+++ model-service/src/scripts/automation.py
@@ -2,57 +2,58 @@ import os
 import subprocess
 import click
 
-
 @click.command()
 @click.option("--name", default="EM_input", help="Name for EasyMocap input.")
 @click.option("--openpose", default="./openpose", help="Path to openpose.")
 @click.option("--cihp", default="./models/CIHP_PGN", help="Path to CIHP_PGN.")
-@click.argument("raw_path")
-@click.argument("intri_path")
-@click.argument("extri_path")
-def automate(name, openpose, cihp, raw_path, intri_path, extri_path):
+@click.option("--raw_path")
+@click.option("--intri_path")
+@click.option("--extri_path")
+@click.argument("projectid")
+def automate(name, openpose, cihp, raw_path, intri_path, extri_path, projectid):
     """Automates the volumetric capture workflow. Creates 3D meshes from raw images."""
     ### Run MakeDatasets
     try:
         # Store output in CIHP_PGN so test_pgn.py does not need to be modified
-        subprocess.check_output(
-            "python scripts/make_dataset.py --delete " + raw_path + " CIHP " + cihp
-        )
+        data_path = 'data/' + projectid
+        output = subprocess.check_output(
+            "python scripts/make_dataset.py " + data_path + " " + "CIHP2 " + data_path
+        , shell=True)
+        print(output)
     except:
         # We would need a cleanup function. And/Or define a checkpoint
         return RuntimeError("Error creating dataset")
 
     ### Run CIHP_PGN
+    """
     try:
-        # Activate CIHP environment
-        subprocess.check_output("source CIHP_ENV/bin/activate", shell=True)
         # CD into CIHP_PGN so relative paths in test_pgn is correct
+        if(cihp is None):
+            cihp = "/models/CIHP"
         os.chdir(cihp)
-        subprocess.check_output("python test_pgn.py", shell=True)
+        subprocess.check_output("python test_pgn.py " + projectid, shell=True)
         # CD back to main directory
-        os.chdir("..")
-        # Deactivate venv
-        subprocess.check_output("deactivate", shell=True)
+        os.chdir("../../")
     except:
         return RuntimeError("CIHP_PGN Error")
-
+    """
+    
     ### Run EasyMocap
     try:
-        # Activate EasyMocap environment
-        subprocess.check_output("source EASY_ENV/bin/activate", shell=True)
         # Create input structure for EasyMocap
         subprocess.check_output(
             "python scripts/raw_to_easymocap.py "
-            + raw_path
+            + data_path
             + " "
             + name
             + " "
-            + intri_path
+            + data_path + "/intrinsic.yml"
             + " "
-            + extri_path
+            + data_path + "/extrinsic.yml"
         )
     except:
         return RuntimeError("Error creating EasyMocap input")
+    """
     try:
         # Generate OpenPose parameters
         subprocess.check_output(
@@ -82,7 +83,7 @@ def automate(name, openpose, cihp, raw_path, intri_path, extri_path):
         subprocess.check_output("deactivate", shell=True)
     except:
         return RuntimeError("Neuralbody Error")
-
+    """
 
 if __name__ == "__main__":
     automate()
