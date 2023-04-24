f871e8732db23773b59f3fba490168bead7548c4
Guillermo Saez <guisaez@email.unc.edu>
2023-04-23 21:02:13 -0400

    Updates

--- common/src/events/model-complete-event.ts
+++ common/src/events/model-complete-event.ts
@@ -1,9 +1,17 @@
 import { Subjects } from "./subjects";
+import { FileTypes } from "./types/file-types";
 
 export interface ModelCompleteEvent {
     subject: Subjects.ProcessComplete;
 
     data: {
-        projectId: string
+        projectId: string,
+        file: {
+            id: string,
+            version: number,
+            userId: string,
+            type: FileTypes,
+            name: string;
+        }
     }
 }
